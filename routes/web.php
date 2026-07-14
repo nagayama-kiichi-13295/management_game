c4408651dc943ef36cb36a34705ffbdadf4b1dd1
kiichi1094 <k.nagayama@tbc-inc.co.jp>
2026-07-14 17:32:30 +0900

    ログイン画面作成 機能はまだ

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// 新規登録
 Route::get('/register', [RegisterController::class, 'show'])
     ->name('register');
 
 Route::post('/register', [RegisterController::class, 'store']);
+
+// ログイン
+Route::get('/login', [LoginController::class, 'show'])
+    ->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+// ログアウト
+Route::post('/logout', [LoginController::class, 'logout'])
+    ->name('logout');
